4a1e4ba0a28ba0e9d993d8c8ff7f89b820680a60
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2021-02-04 18:33:16 -0700

    Updates

---
+++ precache-manifest.6b93b245dd7d7fd52c3c56782775ae57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e4d2b3b700adbc4490b82a6d575c28",
+    "url": "/q-a-react/index.html"
+  },
+  {
+    "revision": "83effbeff8fd2e242d44",
+    "url": "/q-a-react/static/css/main.42a3b694.chunk.css"
+  },
+  {
+    "revision": "957e2a47dd798e2f8b0c",
+    "url": "/q-a-react/static/js/2.64f3b586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/q-a-react/static/js/2.64f3b586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83effbeff8fd2e242d44",
+    "url": "/q-a-react/static/js/main.95977102.chunk.js"
+  },
+  {
+    "revision": "0d3f03b8aa51f13417a0",
+    "url": "/q-a-react/static/js/runtime-main.592e3cbe.js"
+  }
+]);
